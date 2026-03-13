3639e6021b16e82ca1dd9f079ec46b91f3eaf937
jorgeasca <jascola171@gamil.com>
2026-03-13 13:49:00 +0100

    Arreglando estilos

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'https://tracker-apasp.free.beeceptor.com', 
+  baseURL: 'https://tracker-ap.free.beeceptor.com', 
   headers: {
     'Content-Type': 'application/json',
   },
