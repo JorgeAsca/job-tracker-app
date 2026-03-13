daaa6218527192fcce733d0ee8e446946b3211cc
jorgeasca <jascola171@gamil.com>
2026-03-13 11:48:03 +0100

    Error: generacion de pixel art

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    proxy: {
+      // Definimos el prefijo que usaremos en el frontend
+      '/hf-api': {
+        target: 'https://api-inference.huggingface.co', // Destino real de la IA
+        changeOrigin: true,
+        secure: false, // Útil si hay problemas con certificados en local
+        rewrite: (path) => path.replace(/^\/hf-api/, ''), // Eliminamos el prefijo al llegar al destino
+      }
+    }
+  }
 })
